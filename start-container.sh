6b590f2c746ee1ab54ddfddd3a783f755d9161ac
Oskari Noppa <oskari@arpakuut.io>
2021-09-02 00:40:35 +0300

    Make container script work w/ both docker and podman

--- start-container.sh
+++ start-container.sh
@@ -1,9 +1,19 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
 # Build with
-# podman build -t ml-notebook .
+# docker build -t ml-notebook .
 
-podman run -i -t -p 8888:8888 \
+ex="docker"
+if command -v podman &> /dev/null
+then
+  ex="podman"
+elif ! command -v docker &> /dev/null
+then
+  echo "Either Docker or Podman needs to be installed"
+  exit 1
+fi
+
+$ex run -i -t -p 8888:8888 \
   -v .:/notebooks:z ml-notebook /bin/bash \
   -c "/opt/conda/bin/jupyter notebook password && \
   /opt/conda/bin/jupyter notebook \
